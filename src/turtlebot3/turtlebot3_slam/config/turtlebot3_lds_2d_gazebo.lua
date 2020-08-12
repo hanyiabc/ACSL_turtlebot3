6375dcf76bf3d867de2986a7ab0fa019d5431ca9
Yi Han <hanyi@scientiach.com>
2020-08-11 20:56:47 -0400

    sync

--- src/turtlebot3/turtlebot3_slam/config/turtlebot3_lds_2d_gazebo.lua
+++ src/turtlebot3/turtlebot3_slam/config/turtlebot3_lds_2d_gazebo.lua
@@ -31,15 +31,15 @@ options = {
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 0,
-  lookup_transform_timeout_sec = 0.2,
+  lookup_transform_timeout_sec = 0.3,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
-  rangefinder_sampling_ratio = 1.,
-  odometry_sampling_ratio = 1.,
-  fixed_frame_pose_sampling_ratio = 1.,
-  imu_sampling_ratio = 1.,
-  landmarks_sampling_ratio = 1.,
+  rangefinder_sampling_ratio = 0.2,
+  odometry_sampling_ratio = 0.2,
+  fixed_frame_pose_sampling_ratio = 0.2,
+  imu_sampling_ratio = 0.2,
+  landmarks_sampling_ratio = 0.,
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
